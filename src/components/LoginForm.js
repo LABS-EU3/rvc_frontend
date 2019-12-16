101a05d9f11c907f507dac1b862489ee1bba795b
kiyani <bamba.kiyani@gmail.com>
2019-12-16 10:30:30 -0500

    created LoginForm with Formik

---
+++ src/components/LoginForm.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { withFormik, Form, Field } from 'formik';
+import * as Yup from 'yup'; 
+
+function LoginForm(){
+    return (
+        <Form>
+            <Field type='text' name='username' placeholder='Username' />
+            <Field type='password' name='password' placeholder='Password' />
+            <button> Log In </button>
+        </Form>
+    );
+}
+
+const FormikLoginForm = withFormik({
+mapPropsToValues ({username, password }) {
+    return {
+        username: username || '',
+        password: password || ''
+    };
+},
+
+
+handleSubmit(values){
+    console.log(values);
+}
+}) (LoginForm)
+
+
+
+export default FormikLoginForm;
