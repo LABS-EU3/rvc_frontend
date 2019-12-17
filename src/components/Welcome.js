ac66735a621d341763fb77f579b412deef5108f8
Temitope Akinsoto <takinsoto@gmail.com>
2019-12-17 17:55:15 +0100

    Add styles to <Welcome /> component
    In this commit, I added css styles to the <Welcome /> component.
    I created a styled div component and used it to wrap the div being
    returned from the jsx of the <Welcome /> component

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,18 +1,48 @@
 import React from 'react';
+import styles from 'styled-components'
+
+const Div = styles.div`
+background: green;
+display: flex;
+flex-direction: column;
+height: 100vh;
+justify-content: space-evenly;
+align-items: center;
+.title {
+  border: 1px solid white;
+  h1 {
+    font-size: 3rem;
+  }
+}
+.logo-container {
+  border: 1px solid white;
+  img {
+    width: 50%;
+  }
+}
+
+.slogan {
+  border: 1px solid white;
+  h3{
+    font-size: 2.4rem;
+  }
+}
+
+`
 
 function Welcome (){
     return (
-        <div>
+        <Div>
             <div className='title'>
               <h1>FORKBOOK</h1>
             </div>
-            <div className='logo'>
-              <img/>
+            <div className="logo-container">
+              <img src="https://upload.wikimedia.org/wikipedia/commons/6/6f/HP_logo_630x630.png" />
             </div>
             <div className='slogan'>
               <h3>expand your taste</h3>
             </div>
-        </div>
+        </Div>
     );
 }
 
