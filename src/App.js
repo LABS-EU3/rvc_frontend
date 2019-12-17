f6cbdb74c5fd16fa3a649e79eff1d59eeb683edd
chudnofskyATD <mpereannor@gmail.com>
2019-12-17 15:05:39 +0000

    welcome page, recipe (recipe card) and recipeview created and rendering successfully: kiyani, samuel, temi

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import './App.css';
 import FormikLoginForm from './components/LoginForm';
 import FormikRegisterForm from './components/RegisterForm';
 import Welcome from './components/Welcome';
+import RecipeView from './components/RecipeView';
+
 
 function App() {
   return (
     <div className="App">
       <Route exact path='/' component={Welcome}/>
+      <Route path='/' component={RecipeView}/>
       <Route path='/login' component={ FormikLoginForm}/>
       <Route path='/register' component={ FormikRegisterForm}/>
     </div>
