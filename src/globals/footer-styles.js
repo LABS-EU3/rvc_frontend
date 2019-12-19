72b458b320b088b80027612a3fb7ad2141714717
Temitope Akinsoto <takinsoto@gmail.com>
2019-12-19 11:52:43 +0100

    Styling the footer bar

--- src/globals/footer-styles.js
+++ src/globals/footer-styles.js
@@ -1,7 +1,10 @@
 import styled from "styled-components";
 
 export const FooterBar = styled.div`
+background-color: white;
+min-height: 6vh;
+align-items: center;    
+color: white;
 display: flex;
-justify-content: space-between;
-
+justify-content: space-around;
 `
