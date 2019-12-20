8d52ea3220631cd1b43e0af690c5ac259a346afb
Temitope Akinsoto <takinsoto@gmail.com>
2019-12-20 13:45:46 +0100

    Create a <Counter /> component for wiring up a redux store

---
+++ src/components/Couter.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { connect } from "react-redux";
+import * as actionCreators from "./actions";
+
+const Counter = props => {
+  const { count, increment, decrement, reset } = props;
+  return (
+    <div>
+      <p>Count is: {count}</p>
+      <button onClick={increment}>INCREMENT</button>
+      <button onClick={decrement}>DECREMENT</button>
+      <button onClick={reset}>RESET</button>
+    </div>
+  );
+};
+
+function mapStateToProps(state) {
+  return {
+    count: state.count
+  };
+}
+
+export default connect(mapStateToProps, actionCreators)(Counter);
