bc2cd5550e0633b0873ab05ded4e58a0f6f8d264
chudnofskyATD <mpereannor@gmail.com>
2020-01-06 11:42:45 +0000

    first step of create recipe drafted

--- src/App.js
+++ src/App.js
@@ -3,23 +3,17 @@ import {  Route } from 'react-router-dom';
 import './App.css';
 import FormikLoginForm from './components/LoginForm';
 import FormikRegisterForm from './components/RegisterForm';
-import Welcome from './components/Welcome';
 import RecipeView from './components/RecipeView';
 import Counter from './components/Couter'
-import SearchBar from './components/SearchBar';
-import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      {/* <Route exact path='/' component={Welcome}/> */}
       <Route exact path='/' component={RecipeView}/>
       <Route path='/login' component={ FormikLoginForm}/>
       <Route path='/register' component={ FormikRegisterForm}/>
       <Route path='/recipe' component={RecipeView}/>
       <Route path='/counter' component={Counter} />
-      <Route path='/' component={RecipeView}/>
-      <Route exact path='/' component={Footer}/>
     </div>
   );
 }
