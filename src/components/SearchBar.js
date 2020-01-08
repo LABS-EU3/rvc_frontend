2b2452cce0a2f102af2d3b590e65ac39d36213ea
chudnofskyATD <mpereannor@gmail.com>
2020-01-08 14:14:37 +0000

    navigation.js created

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,6 +2,7 @@ import React  from 'react';
 import { SearchBox } from '../globals/navbar-styles';
 import { Link } from 'react-router-dom';
 import { logout } from '../actions';
+import Navigation from './Navigation';
 
 const SearchBar = ({ recipe }) => {
 
@@ -12,30 +13,7 @@ const SearchBar = ({ recipe }) => {
           <input
             type='text'
             placeholder='search recipes...'/>
-          <div className='menu-container'>
-            <nav role='navigation'>
-              <div id='menuToggle'>
-                <input type='checkbox' />
-                <span></span>
-                <span></span>
-                <span></span>
-                <ul id='menu'>
-                <Link to='/' style={{ textDecoration: 'none' }}>
-                  <li>Home</li>
-                  </Link>
-                  <Link to='/register' style={{ textDecoration: 'none' }}>
-                  <li>Profile</li>
-                  </Link>
-                  <Link to='/register' style={{ textDecoration: 'none' }}>
-                  <li>Log in</li>
-                  </Link>
-                  <Link to='/' style={{ textDecoration: 'none' }}>
-                  <li onClick={() => { logout()}}>Logout</li>
-                  </Link>
-                </ul>
-              </div>
-            </nav>
-          </div>
+          <Navigation/>
         </SearchBox>
       </div>
     </div>
