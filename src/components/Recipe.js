2fe9f8c81fbf8e5c2faf1a7bda0224534ee4652b
chudnofskyATD <mpereannor@gmail.com>
2020-01-10 16:40:24 +0000

    author name design atempt 1

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -3,6 +3,7 @@ import '../App.css';
 
 const Recipe = ({ recipe }) => { 
   console.log('AAA', recipe)
+  
   const {
     recipe_title,
     author,
@@ -10,8 +11,9 @@ const Recipe = ({ recipe }) => {
     difficulty,
     budget,
     image, 
-     }  = recipe;
-
+  }  = recipe;
+  
+  console.log('bbb', author)
   let color = '';
   if (difficulty === 1) {
     color ='green';
@@ -26,9 +28,22 @@ const Recipe = ({ recipe }) => {
 // let letterS = 's'
 // const name = 'James'
 // let lastLetter = name.substr(-1)
-
-
 // lastLetter === 's' ? '' : letterS 
+
+const user = "James";
+const lastLetter = user.substr(-1)
+// const remLetters = user.substr(0, user.length-1)
+
+const letterChange = (user) => {
+  if (lastLetter === 's') { 
+    return user + " ' " 
+  }
+  else { 
+    return user + " 's "
+  }
+  // lastLetter === 's' ? user + "'" : user  + "'s"
+
+}
 
 return ( 
   <div className='box'> 
@@ -38,8 +53,9 @@ return (
     <div className='recipe-card'>
       {/* <p>{no_of_likes}</p> */}
       {/* <img className='recipe' alt='an'/> */}
-      {/* <p>James'{letterS}</p> */}
-      <p>{author}'s</p>
+      <p>{author}</p>
+      {/* <p >{letterChange({author.JSON.stringify()})}</p> */}
+      {/* <p>{author}'s</p> */}
       <h2>{recipe_title}</h2>
       <div className='recipe-card-tag'>
         <div>
