aff736f76c6006a99abf886f18a055f7b70abf3a
kiyani <bamba.kiyani@gmail.com>
2020-01-10 02:19:30 -0500

    styled navigation

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,13 +3,20 @@ import { FooterBar } from "../globals/footer-styles";
 import HomeOutlinedIcon from '@material-ui/icons/HomeOutlined';
 import PlayCircleOutlineOutlinedIcon from '@material-ui/icons/PlayCircleOutlineOutlined';
 import PersonOutlineOutlinedIcon from '@material-ui/icons/PersonOutlineOutlined';
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
     <FooterBar>
-      <HomeOutlinedIcon cgit style={{ fontSize: 40, color: 'gray' }} />
-      <PlayCircleOutlineOutlinedIcon color="primary" style={{ fontSize: 40, color: 'gray' }} />
-      <PersonOutlineOutlinedIcon color="primary" style={{ fontSize: 40, color: 'gray' }} />
+      <Link to='/'>
+        <HomeOutlinedIcon cgit style={{ fontSize: 40, color: 'gray' }} />
+      </Link>
+      <Link to='/'>
+        <PlayCircleOutlineOutlinedIcon color="primary" style={{ fontSize: 40, color: 'gray' }} />
+      </Link>
+      <Link to='/profile'>
+        <PersonOutlineOutlinedIcon color="primary" style={{ fontSize: 40, color: 'gray' }} />
+      </Link>
     </FooterBar>
   );
 };
