ca072c8033a99b3d2adcea7799ce01ce0f1d5522
Temitope Akinsoto <takinsoto@gmail.com>
2020-01-11 18:21:13 +0100

    Display the author of a recipe. If the last letter of the author's name ends with letter s, add an apostrophe otherwise add an apostrophe + s to it

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -13,7 +13,6 @@ const Recipe = ({ recipe }) => {
     image, 
   }  = recipe;
   
-  console.log('bbb', author)
   let color = '';
   if (difficulty === 1) {
     color ='green';
@@ -24,26 +23,7 @@ const Recipe = ({ recipe }) => {
   else {
     color = 'red';
   }
-
-// let letterS = 's'
-// const name = 'James'
-// let lastLetter = name.substr(-1)
-// lastLetter === 's' ? '' : letterS 
-
-const user = "James";
-const lastLetter = user.substr(-1)
-// const remLetters = user.substr(0, user.length-1)
-
-const letterChange = (user) => {
-  if (lastLetter === 's') { 
-    return user + " ' " 
-  }
-  else { 
-    return user + " 's "
-  }
-  // lastLetter === 's' ? user + "'" : user  + "'s"
-
-}
+const lastLetter = author.substr(-1) === 's' ? "'" : "'s" ;
 
 return ( 
   <div className='box'> 
@@ -53,9 +33,7 @@ return (
     <div className='recipe-card'>
       {/* <p>{no_of_likes}</p> */}
       {/* <img className='recipe' alt='an'/> */}
-      <p>{author}</p>
-      {/* <p >{letterChange({author.JSON.stringify()})}</p> */}
-      {/* <p>{author}'s</p> */}
+      <p>{author + lastLetter}</p>
       <h2>{recipe_title}</h2>
       <div className='recipe-card-tag'>
         <div>
