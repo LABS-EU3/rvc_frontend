5160361ae4b0d33130ba409ba08a383dd9828a0d
chudnofskyATD <mpereannor@gmail.com>
2020-01-14 12:10:27 +0000

    button style updated

--- src/components/RecipeFormOne.js
+++ src/components/RecipeFormOne.js
@@ -288,7 +288,7 @@ export default function RecipeFormOne(props) {
               <ArrowBackIcon cgit style={{ fontSize: 40, color: 'white' }} />
             </Link>
           </div>
-          <button type='submit' style={{"border":"0 none white "}}>
+          <button type='submit' style={{"border":"none", "background": "inherit", "outline":"none"}}>
           <CheckIcon cgit style={{ fontSize: 40, color: 'white', background:'transparent' }} />
         </button>
         </NavigationSection1>
@@ -421,7 +421,7 @@ export default function RecipeFormOne(props) {
                 }
               }}
             required
-            
+
           />
         </div>
         <div>
