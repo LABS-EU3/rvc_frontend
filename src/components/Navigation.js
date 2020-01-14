2e86c23cf57f6db418d6b75abfafe0b1bc0cd4f7
chudnofskyATD <mpereannor@gmail.com>
2020-01-14 11:39:13 +0000

    navigation updated

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,36 +1,57 @@
-import React  from 'react';
-import { Link } from 'react-router-dom';
-import { logout } from '../actions';
-
-const Navigation = () => { 
-  const isLoggedIn = localStorage.getItem('token');
+import React from "react";
+import { Link } from "react-router-dom";
+import { logout } from "../actions";
+const Navigation = () => {
+  const isLoggedIn = localStorage.getItem("token");
   return (
-    <div className='menu-container'>
-    <nav role='navigation'>
-  <div id='menuToggle'>
-  <input type='checkbox' />
-    <span></span>
-    <span></span>
-    <span></span>
-    <ul id='menu'>
-    <Link to='/' style={{ textDecoration: 'none' }}>
-    <li>Home</li>
-    </Link>
-    <Link to='/profile' style={{ textDecoration: 'none' }}>
-    <li>Profile</li>
-    </Link>
-    <Link to='/createrecipe' style={{ textDecoration: 'none' }}>
-    <li>Create</li>
-    </Link>
-    {isLoggedIn ? <li onClick={() => { logout()}}>Logout</li>  : <li><Link to="/login">Log in</Link></li>}
-    {/* <Link to='/' style={{ textDecoration: 'none' }}>
+    <div className="menu-container">
+      <nav role="navigation">
+        <div id="menuToggle">
+          <input type="checkbox" />
+          <span></span>
+          <span></span>
+          <span></span>
+          <ul id="menu">
+          <li><Link to="/">
+              Home
+            </Link> </li>           
+            {isLoggedIn ? (
+              <div>              
+                <li>
+                <Link to="/createrecipe">
+              Create
+            </Link>
+                </li>
+                <li>
+                <Link to="/profile">
+              Profile
+            </Link>
+                </li>
+                <li
+                  onClick={() => {
+                    logout();
+                  }}
+                >
+                  <Link to="/login">Logout</Link>
+                </li>
+              </div>
+            ) : (
+              <div>
+                <li>
+                  <Link to="/login">Log in</Link>
+                </li>{" "}
+                <li>
+                  <Link to="/register">Register</Link>
+                </li>
+              </div>
+            )}
+            {/* <Link to='/' style={{ textDecoration: 'none' }}>
     <li onClick={() => { logout()}}>Logout</li>
       </Link> */}
-    </ul>
+          </ul>
+        </div>
+      </nav>
     </div>
-    </nav>
-    </div>
-    )
-  }
-  
-  export default Navigation;
+  );
+};
+export default Navigation;
