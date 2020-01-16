9aea891643bd0aed287283e80e131a6429f6be72
CarnunMP <carnun@hotmail.co.uk>
2020-01-16 20:43:51 +0000

    fixed logo_image import in splashView

--- src/components/splashView/Welcome.js
+++ src/components/splashView/Welcome.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import logo_image from '../globals/design-elements/logo_white.png';
+import logo_image from '../../globals/design-elements/logo_white.png';
 import { WelcomeDiv } from './splashView.styles';
 
 function Welcome (){
