4159bb7de973c53458f7951719c925f9f13e531b
James Eneh <erozonachi.james@gmail.com>
2020-01-19 23:25:47 +0100

    Add extended axios utility

---
+++ src/utils/axios.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+export const Axios = () => {
+  return axios.create({
+    headers: {
+      "Content-Type": "application/json"
+    },
+    baseURL: process.env.REACT_APP_API_BASE_URL
+  });
+};
+
+export const axiosWithAuth = () => {
+  return axios.create({
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: localStorage.getItem("token") || ""
+    },
+    baseURL: process.env.REACT_APP_API_BASE_URL
+  });
+};
