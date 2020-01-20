68d1f3f1682b88573788e58c4d5b1a054beae9e4
Temitope Akinsoto <takinsoto@gmail.com>
2020-01-20 13:13:28 +0100

    Add css styles to <LoginError /> and import it to <LoginForm/> component

---
+++ src/components/notification/loginError/loginError.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { StyledDiv } from './loginError.styles'; 
+
+export default function loginError() {
+    return (
+        <StyledDiv>
+            <h2>Invalid username or password!</h2>
+        </StyledDiv>
+    )
+}
---
+++ src/components/notification/loginError/loginError.styles.js
@@ -0,0 +1,9 @@
+import styles from 'styled-components';
+
+export const StyledDiv = styles.div`
+margin-top: 1.3rem;
+height: 20px;
+h2 {
+    color: red;    
+  }
+`
