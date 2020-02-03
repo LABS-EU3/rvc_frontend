4297555926bc64f290d382e2a418c5e3b6e26e74
Temitope Akinsoto <takinsoto@gmail.com>
2020-02-03 14:42:50 +0100

    Add marketing page content

--- src/components/marketingPage/MarketingPage.jsx
+++ src/components/marketingPage/MarketingPage.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React from "react";
 
 export default function MarketingPage() {
-    return (
-        <div>
-            ForkBook Marketing
-        </div>
-    )
+  return (
+    <div>
+      <h1></h1>
+      <div>
+        <img></img>
+        <div className="recipe-details"></div>
+      </div>
+      <div>
+        <button>Start Here</button>
+      </div>
+      <div>
+        <h2>Faster way to manage your recipes</h2>
+      </div>
+      <div>
+        <h2>1 Explore</h2>
+        <p>
+          Discover hundreds of recipes based on the level of difficulty, your
+          budget or time of preparation
+        </p>
+      </div>
+      <div>
+        <h2>2. Innovate</h2>
+        <p>
+          Save - or FORK IT as we like to say - the recipe you like in your own
+          personal cookbook. Edit every forked recipe to your own taste!
+        </p>
+      </div>
+      <div>
+        <h2>3. Create</h2>
+        <p>
+          Create in 4 steps your own recipe and share it on your profile with
+          your followers
+        </p>
+      </div>
+      <footer>
+        <p>About</p>
+        <p>Team</p>
+        <p>Contact</p>
+
+        <p>Terms & conditions | Privacy Policy</p>
+      </footer>
+    </div>
+  );
 }
